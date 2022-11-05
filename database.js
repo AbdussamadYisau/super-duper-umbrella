894d15b20217b76736ea9587482a7b27354a472c
AbdussamadYisau <sammieyisau@gmail.com>
2022-11-05 13:59:57 +0100

    Worked on authentication

--- database.js
+++ database.js
@@ -18,12 +18,6 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         )`, (err) => {
             if (err) {
                 // Table already created
-            } else {
-                // Table just created, creating some rows
-                let insert = 'INSERT INTO user (name, email, password) VALUES (?, ?, ?)'
-                db.run(insert, ["admin", "admin@example.com", md5("admin123456")])
-                db.run(insert, ["user", "user@example.com", md5("user123456")])
-                
             }
         });
     }
--- server.js
+++ server.js
@@ -3,16 +3,33 @@ const app = express();
 const db = require("./database");
 const bodyParser = require("body-parser");
 const md5 = require("md5");
-
+const jwt = require("jsonwebtoken");
+require("dotenv/config");
+const cors = require("cors");
 
 const PORT = 8000;
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+const generateJwtToken = (_id, username, email) => {
+    return jwt.sign(
+      { _id, username, email },
+      process.env.JWT_SECRET_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+  };
 
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-    console.log(`Server running on port: ${PORT}`)
+// Body Parser
+app.use(express.json());
+
+// Middlewares
+app.use(cors());
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`Server running on port: ${process.env.PORT || PORT}`)
 });
 
 app.get("/", (req, res, next) => {
@@ -59,15 +76,22 @@ app.post("/api/user/", (req, res, next) => {
     }
     var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
     var params =[data.name, data.email, data.password]
+
     db.run(sql, params, function (err, result) {
         if (err){
             res.status(400).json({"error": err.message})
             return;
         }
-        res.status(200).json({
-            "message": "success",
-            "data": data,
-            "id" : this.lastID
+        const token = generateJwtToken(this.lastID, data.name, data.email);
+        return res.status(200).json({
+            message: "Signed up successfully",
+            token,
+            data: {
+                id: this.lastID,
+                name: data.name,
+                email: data.email
+            },
+            
         })
     });
 })
@@ -97,22 +121,29 @@ app.post("/api/login", (req, res, next) => {
         }
 
         if (rows.length > 0) {
-            res.status(200).json({
-                "message": "success",
-                "validation": true,
+            const {id, name, email} = rows[0];
+            const token = generateJwtToken(id, name, email)
+            return res.status(200).json({
+                message: "Logged in successfully",
+                token,
+                data: {
+                    id, 
+                    name,
+                    email
+                }
             })
         } else {
-            res.status(200).json({
-                "message": "success",
-                "validation": false,
+            res.status(400).json({
+                status: "Success",
+                message: "No user with these login details",
             })
         }
     })
 })
 
 
-app.use( (req, res) => {
+app.use((req, res) => {
     res.status(404).json({
-        "error": "does not exist"
+        "error": "The resource you are looking for does not exist"
     });
 });
